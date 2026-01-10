254ae6a29a1c8d607e4932c426b798f712022a9b
Yanan2236 <kent31415926535@gmail.com>
2026-01-10 20:10:35 +0900

    feat(admin): restore admin for new croquis session model

--- backend/croquis/admin.py
+++ backend/croquis/admin.py
@@ -1,65 +1,28 @@
-# croquis/admin.py
 from django.contrib import admin
 from .models import (
+    Subject,
     CroquisSession,
-    MotifGroup,
-    MotifTag,
-    SessionTag,
-    Focus,
-    SessionFocus,
-    Image,
+    Drawing,
 )
 
 
-class SessionTagInline(admin.TabularInline):
-    model = SessionTag
-    extra = 0
-    autocomplete_fields = ("tag",)
-    fields = ("tag", "is_primary")
-
-
-class SessionFocusInline(admin.TabularInline):
-    model = SessionFocus
-    extra = 0
-    autocomplete_fields = ("focus",)
-    fields = ("focus",)
-
-
-class ImageInline(admin.TabularInline):
-    model = Image
-    extra = 0
-    fields = ("image_file",)
-
-
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'user', 'created_at')
+    search_fields = ('name', 'user__username')
+    ordering = ('-created_at',)
+    
 @admin.register(CroquisSession)
 class CroquisSessionAdmin(admin.ModelAdmin):
-    list_display = ("id", "owner", "created_at", "duration_seconds")
-    list_filter = ("owner", "created_at")
-    search_fields = ("note",)
-    date_hierarchy = "created_at"
-    ordering = ("-created_at",)
-    inlines = [SessionTagInline, SessionFocusInline, ImageInline]
+    list_display = ('id', 'user', 'subject', 'started_at', 'ended_at', 'is_public', 'created_at')
+    search_fields = ('user__username', 'subject__name')
+    list_filter = ('is_public', 'started_at', 'ended_at')
+    ordering = ('-started_at',)
+    
 
-
-@admin.register(MotifGroup)
-class MotifGroupAdmin(admin.ModelAdmin):
-    list_display = ("id", "owner", "name", "created_at")
-    list_filter = ("owner",)
-    search_fields = ("name",)
-    ordering = ("owner", "name")
-
-
-@admin.register(MotifTag)
-class MotifTagAdmin(admin.ModelAdmin):
-    list_display = ("id", "owner", "group", "name", "created_at")
-    list_filter = ("owner", "group")
-    search_fields = ("name", "group__name")
-    ordering = ("owner", "group__name", "name")
-
-
-@admin.register(Focus)
-class FocusAdmin(admin.ModelAdmin):
-    list_display = ("id", "owner", "title", "created_at")
-    list_filter = ("owner",)
-    search_fields = ("title",)
-    ordering = ("owner", "title")
+    
+@admin.register(Drawing)
+class DrawingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'session', 'created_at')
+    search_fields = ('session__id',)
+    ordering = ('-created_at',)
