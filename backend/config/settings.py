881f36dac0f95afe874cf7d061df5ddcd9a41244
Yanan2236 <kent31415926535@gmail.com>
2026-01-12 19:14:20 +0900

    chore: configure DRF settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -113,3 +113,12 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
