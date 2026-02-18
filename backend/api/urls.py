bc26a6ce52a6d59662b8f9916aa8c0c29b5184cc
Yanan2236 <kent31415926535@gmail.com>
2026-02-18 16:58:42 +0900

    feat(auth): integrate dj-rest-auth email verification and adapter for frontend redirect

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 
+from accounts.views.api.user import csrf
+
 urlpatterns = [
     path('croquis/', include('croquis.urls.api')),
     path('accounts/', include('accounts.urls.api')),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path("auth/csrf/", csrf, name='csrf'),
 ]
