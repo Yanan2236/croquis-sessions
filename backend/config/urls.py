f2485cb66433c58248a5cbc833d058dba7bdfb0d
Yanan2236 <kent31415926535@gmail.com>
2026-03-25 01:46:23 +0900

    feat: move drawing image storage to S3

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,6 +13,3 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('healthz/', healthz)
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
